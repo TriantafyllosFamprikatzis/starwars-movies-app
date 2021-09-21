7019b93f040bd74f9e6d902dd97c823695d6af4f
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2021-09-21 12:26:24 +0300

    Fetch Data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://star-wars-api.herokuapp.com/films');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.map((movieData) => {
+        return {
+          id: movieData.fields.episode_id,
+          title: movieData.fields.title,
+          openingText: movieData.fields.opening_crawl,
+          releaseDate: movieData.fields.release_date,
+          producer: movieData.fields.producer,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
-    <div>Initial Commit</div>
+    <React.Fragment>
+      <section>{content}</section>
+    </React.Fragment>
   );
 }
 
